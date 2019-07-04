3772d63e6eb4bf2d55788a280de352dce55f0217
fdzr <fdzr9106@gmail.com>
2019-07-03 23:27:28 -0400

    Refactoring code, add test and more ...

--- main.rkt
+++ main.rkt
@@ -34,19 +34,17 @@
          | (method <id> (<id>*) <expr>)
 |#
 
-
+; estructura que se va a utilizar para "encapsular" una clase con sus valores asociados
 (struct obj (class values))
 
+; función que representa la clase Padre de la cual todas los objetos van a heredar
+;Object: msg . val -> error o '()
 (define Object
   (λ (msg . vals)
     (case msg
-      [(lookup)     (error "method not found")]
+      [(lookup)(error "method not found")]
       [(all-fields) '()]
-      [(root) #t]
       [else (error "root class: should not happen: " msg)])))
-
-
-
 
 
 (deftype Expr
@@ -154,7 +152,6 @@ Este método no crea un nuevo ambiente.
     [(list 'local (list e ...)  b)
      (lcal (map parse-def e) (parse b))]
     [(list 'class e ...) (class (map parse e) )]
-    ;[(list 'class '<: id e ...)(class id (map parse e))]
     [(list 'send e id b ... )(send (parse e) id (map parse b))]
     [(list 'new e)(new (parse e))]
     [(list 'field x v)(field x (parse v))]
@@ -171,6 +168,14 @@ Este método no crea un nuevo ambiente.
     [(list 'define id b) (my-def id (parse b))]))
 
 ;; interp :: Expr Env -> Val
+; función que toma un expresiçón y la interpreta produciendo un valor o un objeto en caso de que lo devuelva,
+; la opción get interpreta el objeto que le es pasado y devuelve el valor del campo que le es solicitadoñ
+; la opción set interpreta el objeto que le es pasado y modifica el valor del atributo que se le envía con el valor por argumento
+; la opción super envía un mensaje a la clase padre para buscar lo que se le envía por mensaje
+; la opción new crea un objeto
+; la opción class devuelve un objeto con la referencia de la clase y el vector de valores asociados a los campos de las clases
+; la opción send envía un mensaje al objeto que se le pasa como parámetro el método a buscar y los argumentos en caso de que los necesite
+
 (define (interp expr env)
   (match expr
     [(num n) (numV n)]    
@@ -196,76 +201,39 @@ Este método no crea un nuevo ambiente.
        (interp body new-env))     
      ]
     [(class e)
-     
-       (let* ([scls (if(id? (car e))
-                       (interp (cadr e) env)
-                  Object)]
-              ;[fields (append (scls 'all-fields)
-              ;                (list (cons 'f init) ...))]
-              [fields (append (scls 'all-fields)
-                       (if (id? (car e))
-                           (make-fields (cddr e) #t)
-                           (make-fields e #t)))]
-              [methods (make-fields e #f)
-                       #|(local [(defmac (? fd) #:captures self
-                    (vector-ref (obj-values self)
-                                (find-last 'fd fields)))
-                  (defmac (! fd v) #:captures self
-                    (vector-set! (obj-values self)
-                                 (find-last 'fd fields)
-                                 v))
-                  (defmac (--> md . args) #:captures self
-                    (((scls 'lookup 'md) self) . args))]
-            (list (cons 'm (λ (self)
-                            (λ params body))) ...)
-            )|#
-                       ]
-              )
-         (letrec ([class (λ (msg . vals)
-                           (case msg
-                             [(all-fields) fields]
-                             [(create)
-                              (let ([values (list->vector (map cdr fields))])
-                                (begin
-                                  ;(printf "FIELDS ~v~n " fields)
-                                  ;(printf "VALUES ~v~n " values)
-                                  ;(set! fields (append fields (list (cons 'this (obj class values)))))
-                                  ;(printf "~v class root " (scls 'root))
-                                  ;(printf "~v " fields)
-                                  ;(printf "this ~v " (append fields (list (cons 'this (obj class values)))))
-                                  (obj class values)))]
-                             [(read)
+     (let* ([scls (if(id? (car e))
+                     (interp (cadr e) env)
+                     Object)]
+            [fields (append (scls 'all-fields)
+                            (if (id? (car e))
+                                (make-fields (cddr e) #t)
+                                (make-fields e #t)))]
+            [methods (make-fields e #f)]
+            )
+       (letrec ([class (λ (msg . vals)
+                         (case msg
+                           [(all-fields) fields]
+                           [(create)
+                            (let ([values (list->vector (map cdr fields))])
                               (begin
-                                ;(printf "Vals ~v~n" vals)
-                                ;(printf "obj-values ~v~n" (obj-class (first vals)))
-                                (vector-ref (obj-values (first vals))
-                                            (find-last (second vals) fields)))]
-                             [(write)
-                                (vector-set! (obj-values (first vals))
-                                             (find-last (second vals) fields)
-                                             (third vals))]
-                             #;[(invoke)
-                                (let ((method (class 'lookup (second vals))))
-                                  (apply (method (first vals)) (cddr vals)))]
-                             [(field) fields]
-                             [(super)
-                              (scls 'lookup (first vals))]
-                             [(lookup)
-                              (let ([found (assoc (first vals) methods)])
-                                (begin
-                                  ;(printf "~v " methods)
-                                  (if found
-                                      (begin
-                                       ;(printf "valuessss ~v~n " values)
-                                       ;(printf "class ~v~n" (class 'field))
-                                        
-                                      (cons class (cdr found)))
-                                      ;(cdr found)
-                                      (begin
-                                        ;(printf "class ~v~n" (class 'field))
-                                        (scls 'lookup (first vals)))
-                                      )))]))])
-           class))
+                                (obj class values)))]
+                           [(read)
+                            (vector-ref (obj-values (first vals))
+                                        (find-last (second vals) fields))]
+                           [(write)
+                            (vector-set! (obj-values (first vals))
+                                         (find-last (second vals) fields)
+                                         (third vals))]
+                           [(super)
+                            (scls 'lookup (first vals))]
+                           [(lookup)
+                            (let ([found (assoc (first vals) methods)])
+                              (begin
+                                (if found
+                                    (cons class (cdr found))
+                                    (scls 'lookup (first vals))
+                                    )))]))])
+         class))
      ]
     [(get o x)(let ([val ((obj-class (interp o env)) 'read (interp o env) x)])
                 (if (num? val)
@@ -275,30 +243,19 @@ Este método no crea un nuevo ambiente.
     [(new expr)((interp expr env) 'create )]
     [(send o m arg)(let([object (interp o env)])
                      (begin
-                       ;(printf " ~v~n " (cdr))
                        (def cl-body1 ((obj-class object) 'lookup m object))
-                       (printf "vector-full ~v~n" (obj-values object))
-                       ;(printf "Vector ~v~n" (obj-values(car cl-body1)))
-                       ;(printf "bodyyy ~v~n" cl-body1)
                        (def cl-body ((cdr cl-body1)))
-                       ;(printf "cl-body ~v~n" cl-body)
-                       ;(printf "cl-body1 ~v~n" ((car cl-body1)))
-                       ;(printf "~v ~n" (multi-extend-env (car cl-body) arg env))
-                       ;(def cl-body (cadr cl-body1))
-                       ;(printf "~v~n" ((obj-class (car cl-body1)) 'root))
                        (interp (cdr cl-body)
                                (multi-extend-env '(this) (list (obj (car cl-body1) (obj-values object)))
                                                              
-                               (multi-extend-env (car cl-body) (map (lambda(x) (interp x env)) arg) env))) ;(multi-extend-env (car cl-body) arg env)
+                                                 (multi-extend-env (car cl-body) (map (lambda(x) (interp x env)) arg) env))) ;(multi-extend-env (car cl-body) arg env)
                        )
                      )]
     [(super m arg)(let ([object (interp (id 'this) env)])
-                    (printf "super ~v~n " object)
                     (let ([cl-body1 ((obj-class object) 'super m object)])
                       (def cl-body ((cdr cl-body1)))
                       (interp (cdr cl-body) (multi-extend-env (car cl-body) (map (lambda(x) (interp x env)) arg) env)))
-                    
-                   )]
+                    )]
     ))
 
 ;; open-val :: Val -> Scheme Value
@@ -337,14 +294,18 @@ valores de MiniScheme para clases y objetos
     [(num x) x]
     [x x]))
 
-
+; find-last :: symbol lista -> Val o error
+; función que recibe un símbolo a buscar en la lista de campos de la clase, el método devuelve la posición en que se encuentra el campo en la lista y si no está
+; devuelve error
 (define (find-last name alist [rcont -1] [cont 0])
   (match alist
-    ['() (if (equal? rcont -1) (error "field not found:" name) rcont)]
+    ['() (if (equal? rcont -1) (error "field not found") rcont)]
     [(cons (cons f v) t) (if (equal? name f)
                              (find-last name t cont  (+ cont 1))
                              (find-last name t rcont (+ cont 1)))]))
 
+; make-field :: Expr bool -> List[method o fields]
+; función que recibe una expresión y devueve un lista de fields si el parameter es #t y si es #f devuelve una lista de methods
 (define (make-fields l parameter)
   (if(empty? l)
      '()
@@ -357,8 +318,3 @@ valores de MiniScheme para clases y objetos
              (make-fields (cdr l) parameter)))
      ))
 
-(define (my-append l new-values)
-  (if (empty? l)
-      new-values
-      (cons (car l)(my-append (cdr l) new-values)))
-  )
--- tests.rkt
+++ tests.rkt
@@ -23,3 +23,85 @@
 ;                                  SUS TESTS                                  ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(test (run-val '(local
+                  [(define x 10)
+                   (define A
+                     (class
+                         (method m (y) (+ x y))))
+                   (define o (new A))]
+                  (send o m 1))) 11)
+
+(test (run-val '(local
+                  [(define c (class
+                                 (field x 1)
+                               (field y 2)
+                               (method sum (z) (+ (get this x) (+ (get this y) z)))
+                               (method set-x (val) (set this x val))))
+                   (define o (new c))]
+                  (seqn
+                   (send o set-x (+ 1 3))
+                   (+ (send o sum 3) (get o y))))) 11)
+
+(test (run-val '(local
+                  [(define A
+                     (class
+                         (method apply (c)
+                                 (send (new c) m))))
+                   (define o (new A))]
+                  (send o apply (class
+                                    (field x 2)
+                                  (method m () (get this x)))))) 2)
+
+(test (run-val '(local
+                  [(define c1 (class
+                                  (method f (z) (< z 7))))
+                   (define c (class <: c1))
+                   (define o (new c))]
+                  (send o f 20))) #f)
+
+(test (run-val '(local
+                  [(define c2 (class
+                                  (method h (x) (+ x 1))))
+                   (define c1 (class <: c2
+                                (method f () #f)))
+                   (define c (class <: c1
+                               (method g () (super h 10))))
+                   (define o (new c))]
+                  (send o g))) 11)
+
+(test (run-val '(local
+                  [(define A (class
+                                 [field x 1]
+                               [field y 0]
+                               [method ax () (get this x)]))
+                   (define B (class <: A
+                               [field x 2]
+                               [method bx () (get this x)]))
+                   (define b (new B))]
+                  (send b ax))) 1)
+
+(test (run-val '(local
+              [   (define x 10)
+                  (define A
+                    (class
+                        (field c 1)
+                      (field v 2)
+                      (method set (q) (set this c q))
+                      (method get () (get this c))))
+                  (define o (new A))]
+              (seqn
+               (send o set 22)
+               (send o get)
+               )
+              )
+           ) 22)
+
+(test (make-fields
+       (list (field 'x (num 1)) (field 'y (num 0)) (method 'ax '() (get (id 'this) 'x))) #t)
+      (list (cons 'x (num 1)) (cons 'y (num 0))))
+
+(test (find-last 'x '((x . 1) (y . 2) (x . 3) (z . 4))) 2)
+
+(test (Object 'all-fields) '())
+(test/exn (Object 'lookup) "method not found")
+(test/exn (Object 'lookup123) "root class: should not happen:  lookup123")
